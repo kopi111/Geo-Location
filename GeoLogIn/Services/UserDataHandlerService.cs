9e0e88b57170324788bbbc2a59693c2e1356f8e5
Dwayne Aitken <dwayne_aitken@jmmb.com>
2025-10-14 13:45:57 -0500

    first commit

---
+++ GeoLogIn/Services/UserDataHandlerService.cs
@@ -0,0 +1,13 @@
+﻿using GeoLogInDomain.Interface.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLogInApplication.Services
+{
+   public  class UserDataHandlerInfrastructure : IUserDataHandlerInfrastructure
+    {
+    }
+}
---
+++ GeoLogInI/IUserDataHandlerInfrastructure.cs
@@ -0,0 +1,44 @@
+﻿using GeoLogInDomain.DTOs.Request;
+using GeoLogInDomain.DTOs.Responce;
+using GeoLogInDomain.Interface.Infrastructure;
+using GeoLogInDomain.Interface.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLogInInfrastructure
+{
+    public class UserDataHandlerInfrastructure : IUserDataHandlerInfrastructure
+
+    {
+        public Task<Error> checkEmail(Register register)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Error> checkEmailInDataBase(Register register)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Error> checkPassword(Register register)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Error> checkTelephoneNumber(Register register)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Error> checkUserText(Register register)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
+
